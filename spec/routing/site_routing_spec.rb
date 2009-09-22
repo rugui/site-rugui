145f54a3d06a4ebf3e95c6e4ef8b0bdb85ed9c13
Vicente Mundim <vicente.mundim@gmail.com>
2009-09-22 17:08:39 -0300

    Removing unused routes

--- spec/routing/site_routing_spec.rb
+++ spec/routing/site_routing_spec.rb
@@ -5,27 +5,11 @@ describe SiteController do
     it "should map #index" do
       route_for(:controller => "site", :action => "index").should == { :path => "/", :method => :get }
     end
-
-    it "should map #download" do
-      route_for(:controller => "site", :action => "download").should == { :path => "/download", :method => :get }
-    end
-
-    it "should map #community" do
-      route_for(:controller => "site", :action => "community").should == { :path => "/community", :method => :get }
-    end
   end
 
   describe "route recognition" do
     it "should generate params for #index" do
       params_from(:get, "/").should == { :controller => "site", :action => "index" }
-    end
-
-    it "should generate params for #download" do
-      params_from(:get, "/download").should == { :controller => "site", :action => "download" }
-    end
-
-    it "should generate params for #community" do
-      params_from(:get, "/community").should == { :controller => "site", :action => "community" }
     end
   end
 end
