96ed92627c6ec020826bf6c43340d26590b77667
Vicente Mundim <vicente.mundim@gmail.com>
2009-09-22 17:07:47 -0300

    Using external links to github for download and community sections

--- app/helpers/external_routes_helper.rb
+++ app/helpers/external_routes_helper.rb
@@ -1,10 +1,18 @@
 module ExternalRoutesHelper
+  def download_url
+    "http://github.com/rugui/rugui/downloads"
+  end
+
   def documentation_url
     "http://github.com/rugui/rugui/wiki"
   end
 
   def code_url
     "http://github.com/rugui/rugui"
+  end
+
+  def community_url
+    "http://wiki.github.com/rugui/rugui/community"
   end
 
   def blog_url
